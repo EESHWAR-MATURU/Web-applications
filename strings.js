3d17641f1e221df2b8a4c413634926dd2065c5dc
EESHWAR-MATURU <102849365+EESHWAR-MATURU@users.noreply.github.com>
2024-01-27 22:55:23 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,12 @@
+let text = "How 'are' you";
+console.log(text.length);
+let str = "This is eeshwar";
+console.log(str.substring(5, 11));
+let text1 = "I am good";
+console.log(text1.toUpperCase());
+let text3 = "Eeshwar Maturu";
+console.log(text3.toLowerCase());
+console.log(text.concat(" ", text));
+console.log(text.charAt(0));
+console.log(text.indexOf("are"));
+
