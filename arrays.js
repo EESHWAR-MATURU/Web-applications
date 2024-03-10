ce7989343279eb0169c71f97a8e8552f864f95be
Eeshwar <eesrut1@gmail.com>
2024-03-10 19:32:31 +0530

    head

--- arrays.js
+++
@@ -1,8 +0,0 @@
-const Names = ["eeshwar", "lalit", "shreya", "keerthi"];
-console.log(Names.pop());
-const Name = ["vibhakar", "siddu", "lola", "amith"];
-console.log(Name.push("aadi","sid boi"));
-const Nam = ["shriya", "hima", "karthik", "sandy"];
-console.log(Nam.join("/"));
-const Na = ["abhi", "chai", "pranav", "siri"];
-console.log(Na.slice(3));
