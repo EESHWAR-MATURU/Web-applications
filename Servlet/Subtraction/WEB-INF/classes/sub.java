14faf0183147d886fae5e8959939cd2dc336e92f
Eeshwar <eesrut1@gmail.com>
2024-03-21 20:45:24 +0530

    initial commit

---
+++ Servlet/Subtraction/WEB-INF/classes/sub.java
@@ -0,0 +1,15 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class sub extends HttpServlet {
+public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+res.setContentType("text/html");
+PrintWriter pw = res.getWriter();
+
+int x=Integer.parseInt(req.getParameter("t1"));
+int y=Integer.parseInt(req.getParameter("t2"));
+pw.println("Substraction is"+(x-y));
+pw.close();
+}
+}
